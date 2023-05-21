51ba22ec7c786a20f942ee12570033e507cd7227
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-21 16:05:38 +0330

    Update atr.m

--- bot_trader/atr.m
+++ bot_trader/atr.m
@@ -5,12 +5,13 @@ function atr_val = atr(c, p)
 % ATR(1)=cc(2)-cc(3)
 nd = size(c, 1); % nd is No. of rows
 k=1;
-if nd >= p
-    for i=nd:-1:p
+tr = zeros(2,1);
+if nd == p
+    for i=nd:-1:1
         j=i-1;
         if i==1; j=1; end           
         tr(k) = max([c(i,2)-c(i,3), c(i,2)-c(j,4), c(i,3)-c(j,4)]);
         k=k+1;
     end
 end 
-atr_val = tr/p;
+atr_val = sum(tr)/p;
