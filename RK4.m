bfd3f60b13852bcd3af0fda54df950df0106bd22
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-28 19:05:37 +0330

    Update RK4.m

--- RK4.m
+++ RK4.m
@@ -1,5 +1,7 @@
 function [t,x1,x2]=RK4(N,h,t,x11,x21,f1,f2)
+
 t(1)=t;x1(1)=x11;x2(1)=x21;
+
 for i=1:N
     t(i+1)=t(i)+h;
     
@@ -14,6 +16,7 @@ for i=1:N
     
     k4x1=f1(t(i)+h,x1(i)+ h*k3x1,x2(i)+h*k3x2);
     k4x2=f2(t(i)+h,x1(i)+ h*k3x1,x2(i)+h*k3x2);
+
     
     x1(i+1)=x1(i)+(h/6)*(k1x1+2*k2x1+2*k3x1+k4x1);
     x2(i+1)=x2(i)+(h/6)*(k1x2+2*k2x2+2*k3x2+k4x2);
