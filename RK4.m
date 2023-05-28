17440df15c946fcdd1b1c02adff0541c84487ecf
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-28 19:24:35 +0330

    Update RK4.m

--- RK4.m
+++ RK4.m
@@ -6,19 +6,24 @@ for i=1:N
     t(i+1)=t(i)+h;
     
     k1x1=f1(t(i),x1(i),x2(i));
+
     k1x2=f2(t(i),x1(i),x2(i));
     
     k2x1=f1(t(i)+h/2,x1(i)+(h/2)*k1x1,x2(i)+(h/2)*k1x2);
+
     k2x2=f2(t(i)+h/2,x1(i)+(h/2)*k1x1,x2(i)+(h/2)*k1x2);
     
     k3x1=f1(t(i)+h/2,x1(i)+(h/2)*k2x1,x2(i)+(h/2)*k2x2);
+
     k3x2=f2(t(i)+h/2,x1(i)+(h/2)*k2x1,x2(i)+(h/2)*k2x2);
     
     k4x1=f1(t(i)+h,x1(i)+ h*k3x1,x2(i)+h*k3x2);
+
     k4x2=f2(t(i)+h,x1(i)+ h*k3x1,x2(i)+h*k3x2);
 
     
     x1(i+1)=x1(i)+(h/6)*(k1x1+2*k2x1+2*k3x1+k4x1);
     x2(i+1)=x2(i)+(h/6)*(k1x2+2*k2x2+2*k3x2+k4x2);
 end
+
 end
